9ddbad7be06bd510faf55665c6a8175f92e0472d
felpsalvs <profelipealves@outlook.com>
2024-09-24 11:40:46 -0300

    update app.tsx

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from "@tanstack/react-query";
+import { Dialog } from './components/ui/dialog'
+import { CreateGoal } from './components/create-goal'
+import { Summary } from './components/summary'
+import { EmptyGoals } from './components/empty-goals'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App(){
+  const {data} = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60,
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Dialog>
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
+
+      <CreateGoal/>
+    </Dialog>
   )
 }
-
-export default App
