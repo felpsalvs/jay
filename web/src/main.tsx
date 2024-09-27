15c78a42448fd27f4f47ff211bb6d6182da6519d
felpsalvs <profelipealves@outlook.com>
2024-09-26 23:33:09 -0300

    chore: updated format code

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,13 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import App from './app.tsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createRoot } from "react-dom/client";
+import App from "./app.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import './index.css'
+import "./index.css";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
-)
+createRoot(document.getElementById("root")!).render(
+	<QueryClientProvider client={queryClient}>
+		<App />
+	</QueryClientProvider>,
+);
